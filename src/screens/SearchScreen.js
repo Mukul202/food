a6fac5a4efef997b48f8f7db5dfbd56fb928dbc3
Mukul Kadyan <mukulkadyan202@gmail.com>
2022-06-04 21:14:11 +0530

    Add files via upload

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { View,Text,StyleSheet,ScrollView } from "react-native";
+import ResultsList from "../components/ResultsList";
+import SearchBar from "../components/SearchBar";
+
+import useResults from "../hooks/useResults";
+
+const SearchScreen = () => {
+
+  const [term,setTerm]=useState('');
+
+  const [results,searchApi,error]=useResults();
+
+  const filterResultsByPrice = (price) => {
+    //price === '$' || '$$' || '$$$'
+    return results.filter(result => result.price===price);
+  };
+
+
+  return (
+    <>
+      <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)} onTermSubmit={() => searchApi(term)} />
+      {error ? <Text>{error}</Text>:null}
+      <ScrollView>
+        <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+        <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+        <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
+      </ScrollView>
+    </>
+  )
+}
+
+const styles=StyleSheet.create({
+});
+
+export default SearchScreen;

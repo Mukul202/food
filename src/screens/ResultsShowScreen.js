514ed54b08aa7da9ce5222896157299de29d1f9f
Mukul Kadyan <mukulkadyan202@gmail.com>
2022-06-04 21:13:21 +0530

    Create ResultsShowScreen.js

---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,57 @@
+import React,{useEffect, useState} from "react";
+import {View,Text,StyleSheet,FlatList,Image} from "react-native";
+import yelp from "../api/yelp";
+
+
+const ResultsShowScreen = ({navigation}) => {
+
+  const [result,setResult] = useState(null);
+
+  const id=navigation.getParam('id');
+
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  // console.log(result);
+
+  useEffect(() => {
+    getResult(id);
+  },[]);
+
+  if(!result){
+    return null;
+  }
+
+  return (
+    <>
+      <Text style={styles.heading}>{result.name}</Text>
+      <FlatList 
+        data={result.photos}
+        keyExtractor = {photo => photo}
+        renderItem= {({item}) => {
+          return (
+            <Image style={styles.image} source={{uri:item}} />
+          )
+        }}
+        showsVerticalScrollIndicator={false}
+      />
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  image:{
+    height:200,
+    width:300,
+    borderRadius:5,
+    margin:5,
+  },
+  heading:{
+    fontWeight:'bold',
+    marginLeft:5,
+  }
+});
+
+export default ResultsShowScreen;
